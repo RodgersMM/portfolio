8e080c769a452b6d1c6550910ddd4a26d713622b
rodgersmm <rodgersmutua44@gmail.com>
2023-07-26 12:19:48 +0300

    Add React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Image from './components/Image';
+import Imagedsc from './components/Imagedsc';
+import Hobbies from './components/Hobbies';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Image />
+      <Imagedsc />
+
+      <About />
+      <Hobbies />
+
+      <Skills />
+      <Projects />
+      <Footer />
+
+        
+     
     </div>
   );
 }
