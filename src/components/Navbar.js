f76db7894dae1d46518a2ba9fe9bac44f36ac27f
rodgersmm <rodgersmutua44@gmail.com>
2023-07-27 13:45:39 +0300

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,18 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-500 p-4">
       <div className="flex items-center justify-between">
+
         <div className="text-white font-bold text-xl">PORTFOLIO LANDING PAGE</div>
-        <div className='flex text-white font-bold justify-around text-xl'>
-          <div className='items-center'>TOP SKILLS:</div>
-          <div className='items-center'>Web Dev</div>
-          <div className='items-center'>Agile</div>
-          <div className='items-center'>SEO</div>
-          <div className='items-center'>SMM</div>
+
+        <div className='flex items-center text-white font-bold justify-between text-xl'>
+          <ul className='flex justify-evenly items-center ' >
+            <li className='items-center'>TOP SKILLS: </li>
+            <li className='items-center'>WEB DEV | </li>
+            <li className='items-center'>AGILE |</li>
+            <li className='items-center' >SEO |</li>
+            <li className='items-center'>SMM</li>
+
+          </ul>
       </div>
       </div>
     </nav>
